c793aacadfb4b705059d91651a28a70f1f293630
Gabriel Bonner <gabrielbonner@gmail.com>
2016-07-26 17:58:29 -0700

    Add test marker to seed file, modify column name in markers migration

--- db/migrate/20160727002306_create_markers.rb
+++ db/migrate/20160727002306_create_markers.rb
@@ -3,7 +3,7 @@ class CreateMarkers < ActiveRecord::Migration
     create_table :markers do |t|
       t.string   :name
       t.string   :description
-      t.string   :tags
+      t.string   :tagwords
       t.string   :latitude
       t.string   :longitude
       t.text     :infowindow
